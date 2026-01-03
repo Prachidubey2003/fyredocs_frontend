6ca9312eec86000dd12b23159ea514430230b949
Shivam Dubey <condingfarms@gmail.com>
2026-01-03 17:47:30 +0000

    Changes

---
+++ src/pages/PdfToImagePage.tsx
@@ -0,0 +1,7 @@
+import { ConvertTool } from '@/components/tools/ConvertTool';
+
+const PdfToImagePage = () => {
+  return <ConvertTool toolId="pdf-to-image" outputFormat="png" />;
+};
+
+export default PdfToImagePage;
